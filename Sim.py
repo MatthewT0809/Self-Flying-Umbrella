cf6574b70a0980a1eb1a4e3c9fd2d10dbb104f23
Matthew Tam <matthewchtam@gmail.com>
2024-07-10 19:03:14 +0800

    added PID code

--- Sim.py
+++ Sim.py
@@ -6,11 +6,11 @@ import time
 # ------
 # Global Parameters
 TIMER = 0
-TIME_STEP = 0.01  # seconds
-SETPOINT = 100
-SIM_TIME = 10  # seconds
+TIME_STEP = 0.005  # seconds
+SETPOINT = 10
+SIM_TIME = 10 *1000  # seconds
 INITIAL_X = 0
-INITIAL_Y = 0
+INITIAL_Y = -100
 V_i = 0  # inital velocity
 Y_i = 0  # inital height
 MASS = 1  # kg
@@ -25,24 +25,24 @@ class Simulation(object):
         self.screen.setup(1280,900)
         self.marker = turtle.Turtle()
         self.marker.penup()
-        self.marker.left(90)
+        self.marker.left(180)
         self.marker.goto(15,SETPOINT)
         self.marker.color('red')
         self.sim = True
         self.timer = 0
     def cycle(self):
         while(self.sim):
-            thrust = 5
+            thrust = 10
             self.Insight.set_ddy(thrust) 
             self.Insight.set_dy()
             self.Insight.set_y()
-            time.sleep(TIME_STEP)
-            time += 1
+            time.sleep(TIME_STEP)  
+            self.timer += 1
             if self.timer > SIM_TIME: 
                 self.sim = False
-            elif self.Insight.get_y > 800: 
+            elif self.Insight.get_y() > 800: 
                 self.sim = False
-            elif self.Insight.get_y < -800:
+            elif self.Insight.get_y() < -800:
                 self.sim = False 
 
 class Rocket(object):
@@ -51,12 +51,13 @@ class Rocket(object):
         self.Rocket = turtle.Turtle()
         self.Rocket.shape('square')
         self.Rocket.color('black')
+        self.Rocket.penup()
         self.Rocket.goto(INITIAL_X,INITIAL_Y)
         self.Rocket.speed(0)
         #physics
         self.ddy = 0
         self.dy = V_i
-        self.y = Y_i
+        self.y = INITIAL_Y
     def set_ddy(self,thrust):
         self.ddy = g + thrust / MASS
     def get_ddy(self):
@@ -68,6 +69,7 @@ class Rocket(object):
     def set_y(self):
         self.Rocket.sety(self.y + self.dy)
     def get_y(self):
+        self.y = self.Rocket.ycor()
         return self.y
 
 def main():
